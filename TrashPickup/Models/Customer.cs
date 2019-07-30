36fe6a32a466d4def0aa9d93a96f7243e13cbb67
Vincent Mackay <mackv42@gmail.com>
2019-07-30 14:09:22 -0500

    created Customer and Employee models

---
+++ TrashPickup/Models/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrashPickup.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
+    namespace TrashPickup.Models
+    {
+        public class Customer
+        {
+            string Email { get; set; }
+            string Street { get; set; }
+            string Address { get; set; }
+            string State { get; set; }
+            string Zip { get; set; }
+            sbyte pickupDay { get; set; }
+            DateTime suspendStart { get; set; }
+            DateTime suspendEnd { get; set; }
+            DateTime OneTimePickup { get; set; }
+        }
+    }
+}
