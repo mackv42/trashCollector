8b0ed53c9323c0b4c64039aba426b4663f46b6e9
Vincent Mackay <mackv42@gmail.com>
2019-07-30 13:47:44 -0500

    init

---
+++ TrashPickup/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrashPickup.Models
+{
+    public class Employee
+    {
+        string Name { get; set; }
+        string Zip { get; set; }
+    }
+}
---
+++ TrashPickup/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrashPickup.Startup))]
+namespace TrashPickup
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
